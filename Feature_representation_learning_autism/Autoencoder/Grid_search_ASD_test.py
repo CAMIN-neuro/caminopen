69f3234a89e1ec2e111a8fd6b6ffecaf419aa181
CAMIN-neuro <36185585+CAMIN-neuro@users.noreply.github.com>
2023-11-13 21:52:57 +0900

    Update Grid_search_ASD_test.py

--- Feature_representation_learning_autism/Autoencoder/Grid_search_ASD_test.py
+++ Feature_representation_learning_autism/Autoencoder/Grid_search_ASD_test.py
@@ -120,7 +120,7 @@ from tqdm import tqdm
 
 
 # LeakyReLU ReLU Tanh
-# Tanh 고정 제일 performance 좋음
+# Tanh: best performance
 
 class AutoEncoder(nn.Module):
     def __init__(self, dropout, layer, unit_):
